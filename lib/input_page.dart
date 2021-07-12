22dc6dc795c0cc41f3874eeb99924e65ab3cd597
amrel7afy <amr.el7afy@gmail.com>
2021-07-12 03:27:53 +0200

    slider and weight card is added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'reusable_card.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -15,6 +16,8 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender = Gender.male;
+  int height = 180;
+  int weight =70;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,36 +69,115 @@ class _InputPageState extends State<InputPage> {
             ),
           ),
           Expanded(
-            child: ReusableCard(colour: kActiveColor, cardChild: Column(
-              children: [
-                Text('HEIGHT',style: kLabelStyle,)
-              ],
-            )),
+            child: ReusableCard(
+                colour: kActiveColor,
+                cardChild: Column(
+                  children: [
+                    Text(
+                      'HEIGHT',
+                      style: kLabelStyle,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: [
+                        Text(
+                          height.toString(),
+                          style: kNumberStyle,
+                        ),
+                        Text(
+                          'cm',
+                          style: kLabelStyle,
+                        )
+                      ],
+                    ),
+                    SliderTheme(
+                      data: SliderThemeData().copyWith(
+                          activeTrackColor: Colors.white,
+                          inactiveTrackColor: kInactiveColor,
+                          thumbColor: kBottomContainerColor,
+                          overlayColor: Color(0x29EB1555),
+                          trackHeight: 1),
+                      child: Slider(
+                        min: 120,
+                        max: 220,
+                        onChanged: (double value) {
+                          setState(() {
+                            height = value.round();
+                          });
+                        },
+                        value: height.toDouble(),
+                      ),
+                    )
+                  ],
+                )),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
                   child: ReusableCard(
-                      colour: Colors.white, cardChild: Column(
-                    children: [
-                      Text('HEIGHT',style: kLabelStyle,)
-                    ],
-                  )),
+                      colour: kActiveColor,
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'WEIGHT',
+                            style: kLabelStyle,
+                          ),
+                          Text(weight.toString(),style: kNumberStyle,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RoundBotton(icon: FontAwesomeIcons.minus,
+                                onpress: (){
+                                  setState(() {
+                                    weight--;
+                                  });
+                                },
+                              ),
+                              SizedBox(width: 10,),
+                              RoundBotton(icon: FontAwesomeIcons.plus,
+                                  onpress: (){
+                                    setState(() {
+                                      weight++;
+                                    });
+                                  })
+                            ],
+                          )
+                        ],
+                      )),
                 ),
                 Expanded(
                     child: ReusableCard(
-                        colour: Colors.white, cardChild: Container()))
+                        colour: kActiveColor, cardChild: Container()))
               ],
             ),
           ),
           Container(
-            color: Color(0xffEB1555),
+            color: kBottomContainerColor,
             width: double.infinity,
             height: 80,
           )
         ],
       ),
     );
+  }
+}
+
+class RoundBotton extends StatelessWidget {
+RoundBotton({required this.onpress,required this.icon});
+final Function onpress;
+final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      constraints: BoxConstraints.tightFor(width: 56.0,height: 56.0),
+      fillColor:  Color(0xff212747),
+      elevation: 5,
+      shape: CircleBorder(),
+        onPressed: (){onpress();});
   }
 }
